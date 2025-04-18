994ed9e2c27d5d7d8c87f767ff3e65c014bd636b
Shantanu Shirkar <113785315+ShantanuSkrr@users.noreply.github.com>
2025-04-19 00:33:32 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_qC3sOB5iG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '31tndtrclb8h3evi25l0svvsrd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_vM03XUx9S', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7i7dmbc66rb59jqo3f9bvljb3c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
